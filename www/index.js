b52187728d0efbed4275a824e4f6669c4645f582
Muhammad Umer Azad <umer@fastmail.com>
2019-11-14 23:07:58 -0800

    Added FPS for debuggin

--- www/index.js
+++ www/index.js
@@ -24,7 +24,49 @@ const ctx = canvas.getContext('2d');
 
 let animationId = null;
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimestamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimestamp;
+        this.lastFrameTimestamp = now;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+
+        for (let i = 0; i < this.frames.length; i++) {
+            let f = this.frames[i];
+            sum += f;
+            min = Math.min(min, f);
+            max = Math.max(max, f);
+        }
+
+        let mean = sum / this.frames.length;
+
+        this.fps.textContent = `
+     Frames per Second:
+            latest = ${Math.round(fps)}
+   avg of last 100 = ${Math.round(mean)}
+   min of last 100 = ${Math.round(min)}
+   max of last 100 = ${Math.round(max)}
+        `.trim();
+    }
+}
+
 const renderLoop = () => {
+    fps.render();
     drawGrid();
     drawCells();
     universe.tick();
